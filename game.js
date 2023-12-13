818972f270c458d9f557cc816cd967745c2b0743
L4444 <38132821+L4444@users.noreply.github.com>
2023-12-13 19:54:39 +0000

    Rotation to Cursor
    This update I have added some basic enemy ship sprites and now the player rotates to cursor.
    
    Pretty neat :+1:

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
 var config = {
     type: Phaser.AUTO,
-    width: 1440,
+    width: 900,
     height: 900,
     physics: {
             default: 'arcade',
             arcade: {
                 x: 0,
                 y: 0,
-                width: 1440,
+                width: 900,
                 height: 900,
                 debug: true
             }
@@ -28,9 +28,14 @@ var explosion;
 var music;
 var BIG_THRUST = 1000;
 var LITTLE_THRUST = 5.0;
+
+var enemy = [];
+
+
 function preload ()
 {
-    this.load.image('ship','ships/1.png');
+    this.load.image('player','ships/1B.png');
+    this.load.image('enemy','ships/8.png');
     this.load.image('back','Backgrounds/Blue Nebula/Blue Nebula 1 - 1024x1024.png');
 
     var u;
@@ -61,9 +66,17 @@ function create ()
     explosion = this.add.sprite(400,400, 'boom14');
     explosion.setScale(0.25);
 
-    player = this.physics.add.sprite(40,400, 'ship');
+    player = this.physics.add.sprite(40,400, 'player');
     player.body.setCollideWorldBounds(true);
     player.setScale(0.5);
+
+    for(let i = 0;i <3;i++)
+    {
+        enemy[i] = this.physics.add.sprite(400 * i,400, 'enemy');
+        enemy[i].body.setCollideWorldBounds(true);
+        enemy[i].setScale(0.5);
+        enemy[i].angle = 180;
+    }
    
 
     keys = this.input.keyboard.addKeys('W,S,A,D,F');
@@ -143,13 +156,28 @@ function update ()
     if(player.body.velocity.x > 400) {player.setVelocityX(400);tX = 0;}
     if(player.body.velocity.x < -400) {player.setVelocityX(-400);tX = 0;}
 
-    // Rotating the ship gives the player feedback on moving.
-    player.angle = player.body.velocity.x/10;
+
     // Activate big thruster!
     player.setAcceleration(tX,tY);
 
+
+
+    // Angle the ship to "look at" the cursor, Cursor aiming
+    let targetAngle = Phaser.Math.RadToDeg(
+        Phaser.Math.Angle.Between(player.x, player.y, game.input.mousePointer.x,game.input.mousePointer.y)
+        ) + 90; // The +90 is to ensure it points forward rather than to the right.
+    
+
+    
+
+    // TEST: Just set it
+    player.angle = targetAngle;
+
     // Present debug info
-    infoText.setText("Big thrust is " + BIG_THRUST + "\nand Little thrust is " + LITTLE_THRUST + "\nVelX = " + player.body.velocity.x + "\nVelY = " + player.body.velocity.y);
+    infoText.setText("Big thrust is " + BIG_THRUST + "\nand Little thrust is " + LITTLE_THRUST + "\nVelX = " + player.body.velocity.x + "\nVelY = " + player.body.velocity.y
+    + "\nCursorX: " + game.input.mousePointer.x + "\nCursorY: " + game.input.mousePointer.y + "\ntargetAngle: " + targetAngle + "\nPlayer Angle: " + player.angle);
+
+    
 
 
     // Cheesy scrolling background
